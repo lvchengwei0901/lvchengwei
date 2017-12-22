ea842cb374b6f828967ecf53d3e4f10bfca6c88e
lvchengwei <15925656279@163.com>
2017-12-22 09:07:23 +0800

    2

--- src/main/java/com/lcw/controller/UserController.java
+++ src/main/java/com/lcw/controller/UserController.java
@@ -12,10 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.lcw.constants.Constants;
 import com.lcw.entity.User;
